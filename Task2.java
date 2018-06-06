59129a1405fcf1c14ef3484437b5da1b2631756b
ViktoryiaTamasheuskaya <39959716+ViktoryiaTamasheuskaya@users.noreply.github.com>
2018-06-07 01:13:41 +0300

    Update Task2.java
    Changed  encoding reference

--- Task2.java
+++ Task2.java
@@ -2,9 +2,7 @@ package Tasks;
 
 public class Task2 {
 	public static void main (String[] args) {
-		
-	System.out.println("ФИО - Томашевская Виктория Евгеньевна," + " Адрес - Минск, ул.Центральная, д.1," + " Мой телефон - 029000000");
-	
+	System.out.println("Viktoryia Tamasheuskaya \nZhukava street, 18 \n+375-29-2226213");
 	}
 
 }
